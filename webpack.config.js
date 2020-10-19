d07e0f1f741dea2a323a9eee8518a88231e574f3
Frenzy265 <klaus_franziska@gmx.de>
2020-10-19 16:35:22 +0200

    Load images

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,11 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
     ],
   },
 };
